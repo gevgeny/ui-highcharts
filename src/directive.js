1bb4574c4f68c980a9ad2d9e661aa0e338fb1d19
e.gluhotorenko <eugene.gluhotorenko@ctco.lv>
2014-05-30 15:06:48 +0300

    add extensions serice

--- src/directive.js
+++ src/directive.js
@@ -1,119 +1,50 @@
 !function () {
 
-    var createDefaultOptions = function ($scope, $element, $attrs) {
-        var options = {
-            chart : { renderTo : $element[0] },
-            plotOptions : {}
+    var createDefaultOptions = function ($element) {
+        return {
+            chart : { renderTo : $element[0] }
         };
-
-        if ($attrs.start && $attrs.end) {
-            $.extend(true, options, {
-                xAxis: {
-                    events: {
-                        setExtremes: function (event) {
-                            if ($scope.start !== event.min || $scope.end !== event.max) {
-                                $scope.start = event.min;
-                                $scope.end = event.max;
-                                $timeout(function() {
-                                    $scope.$apply();
-                                });
-                            }
-                        }
-                    }
-                }
-            });
-        }
-
-        if ($attrs.pointClick) {
-            $.extend(true, options.plotOptions, {
-                series: {
-                    point : {
-                        events: {
-                            click: function () {
-                                $scope.pointClick({ point : this });
-                            }
-                        }
-                    }
-                }
-            });
-        }
-
-        if ($attrs.legendItemClick) {
-            $.extend(true, options.plotOptions, {
-                series: {
-                    events: {
-                        legendItemClick: function () {
-                            $scope.legendItemClick({ series: this });
-                            return false;
-                        }
-                    }
-                }
-            });
-        }
-
-        return options;
-    };
-
-    var addLegendHover = function (chart, $element) {
-        $element.find('.highcharts-legend').on('mouseenter mouseleave', '.highcharts-legend-item', function (event) {
-            if (event.type === 'mouseenter') {
-                chart.series[$(this).index()].data[0].onMouseOver();
-            } else {
-                chart.series[$(this).index()].data[0].onMouseOut();
-            }
-        });
     };
 
     var createDirective = function (type) {
-        var directive = function (watchHelper, formatter) {
-            return {
-                restrict: 'EAC',
-                transclude: true,
-                replace: true,
-                template: '<div></div>',
-                scope: {
-                    options: '=?',
-                    series: '=',
-                    redraw: '=',
-                    loading: '=',
-                    start: '=',
-                    end: '=',
-                    pointClick: '&',
-                    legendItemClick: '&'
-                },
-                link: function ($scope, $element, $attrs, $ctrl, $transclude) {
-                    var defaultOptions = createDefaultOptions($scope, $element, $attrs),
-                        optionsWithFormatters = formatter.applyFormatters($scope, $transclude),
-                        chart;
+        return [ '_uiHighchartsWatchHelperService',  '_uiHighchartsFormatterService', '_uiHighchartsExtensionsService',
+            function (watchHelper, formatter, extensions) {
+                return {
+                    restrict: 'EAC',
+                    transclude: true,
+                    replace: true,
+                    template: '<div></div>',
+                    scope: {
+                        options: '=?',
+                        series: '=',
+                        redraw: '=',
+                        loading: '=',
+                        start: '=',
+                        end: '=',
+                        pointClick: '&',
+                        legendItemClick: '&'
+                    },
+                    link: function ($scope, $element, $attrs, $ctrl, $transclude) {
+                        var defaultOptions = { chart : { renderTo : $element[0] } },
+                            optionsWithFormatters = formatter.applyFormatters($scope, $transclude),
+                            chart;
 
-                    $scope.options = $scope.options || {};
+                        $scope.options = $scope.options || {};
 
-                    $.extend(true, $scope.options, optionsWithFormatters, defaultOptions);
-                    chart = new Highcharts[type]($scope.options);
+                        $.extend(true, $scope.options, optionsWithFormatters, defaultOptions);
+                        chart = new Highcharts[type]($scope.options);
 
-                    watchHelper.watchSeriesLength(chart, $scope);
-                    watchHelper.watchChartRedraw(chart, $scope, $attrs);
-                    watchHelper.watchDisabling(chart, $scope);
-                    watchHelper.watchLoading(chart, $scope);
-                    watchHelper.watchAxis(chart, $scope, 'xAxis');
-                    watchHelper.watchAxis(chart, $scope, 'yAxis');
+                        watchHelper.watchSeriesLength(chart, $scope);
+                        watchHelper.watchChartRedraw(chart, $scope, $attrs);
+                        watchHelper.watchDisabling(chart, $scope);
+                        watchHelper.watchLoading(chart, $scope);
+                        watchHelper.watchAxis(chart, $scope, 'xAxis');
+                        watchHelper.watchAxis(chart, $scope, 'yAxis');
 
-                    if ('trackTooltip' in $attrs) {
-                        addLegendHover(chart, $element);
+                        extensions.addLegendHover(chart, $element, $attrs);
                     }
-
-                    if ($scope.start && $scope.end) {
-                        $scope.$watchCollection('[start, end]', function () {
-                            chart.xAxis[0].setExtremes($scope.start, $scope.end);
-                        });
-                    }
-                }
-            };
-        };
-
-        directive.$inject = ['_uiHighchartsWatchHelperService', '_uiHighchartsFormatterService'];
-
-        return directive;
+                };
+        }];
     };
 
     angular.module('ui-highcharts')
